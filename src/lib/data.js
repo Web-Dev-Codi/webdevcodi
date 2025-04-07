c3e6c5e95189f9f188a515cc7f71e2ee9ff56a24
Web-Dev-Codi <cordiscobrian@gmail.com>
2025-04-07 15:31:37 +0200

    created data file for showcase

---
+++ src/lib/data.js
@@ -0,0 +1,82 @@
+export const data = [
+  {
+    id: 1,
+    title: "Inquizable",
+    stackImages: [
+      "/icons/SkillIconsJavascript.svg",
+      "/icons/SkillIconsTailwindcssLight.svg",
+      "/icons/SkillIconsReactDark.svg",
+      "/icons/SkillIconsNodejsDark.svg",
+    ],
+    imgUrl: "/images/inquizable.jpg",
+    description:
+      "Test your general knowledge with this quiz app. User log-in,registration and profile page.",
+    href: "https://www.inquizable.com/",
+  },
+  {
+    id: 2,
+    title: "GitHub Finder",
+    stackImages: [
+      "/icons/SkillIconsJavascript.svg",
+      "/icons/SkillIconsViteDark.svg",
+      "/icons/SkillIconsReactDark.svg",
+      "/icons/SkillIconsGithubLight.svg",
+    ],
+    imgUrl: "/images/github.jpg",
+    description:
+      "Search Githubs database for users, and display repos, stars, and achievements.",
+    href: "https://git-hub-finder-vite.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Four Flavours Express",
+    stackImages: [
+      "/icons/SkillIconsJavascript.svg",
+      "/icons/SkillIconsTailwindcssLight.svg",
+      "/icons/SkillIconsReactDark.svg",
+    ],
+    imgUrl: "/images/ffe.png",
+    description:
+      "Four Flavours Express is a food delivery service that allows you to have your favourite meals delivered to your door.",
+    href: "https://fourflavoursexpress.onrender.com/",
+  },
+  {
+    id: 4,
+    title: "Diet-Tracker",
+    stackImages: [
+      "/icons/SkillIconsJavascript.svg",
+      "/icons/SkillIconsCss.svg",
+      "/icons/SkillIconsHtml.svg",
+    ],
+    imgUrl: "/images/diettracker.jpg",
+    description:
+      "Track calories burnt from exercise and dieting. Meals and workouts are calculated from daily expenditure.",
+    href: "https://dietracker.vercel.app/",
+  },
+  {
+    id: 5,
+    title: "Text to Image Generator",
+    stackImages: [
+      "/icons/SkillIconsJavascript.svg",
+      "/icons/SkillIconsTailwindcssLight.svg",
+      "/icons/SkillIconsReactDark.svg",
+    ],
+    imgUrl: "/images/ai.jpg",
+    description: "Generate a custom image from text within seconds using AI.",
+    href: "https://texttoimagegenerator.vercel.app/",
+  },
+  {
+    id: 6,
+    title: "Flixx Movie Database",
+    stackImages: [
+      "/icons/SkillIconsJavascript.svg",
+      "/icons/SkillIconsCss.svg",
+      "/icons/SkillIconsHtml.svg",
+      "/icons/SkillIconsWebpackLight.svg",
+    ],
+    imgUrl: "/images/flixx.webp",
+    description:
+      " Search a movie database for new releases, ratings and possible viewing pleasure",
+    href: "https://flixxmoviesearchengine.vercel.app/",
+  },
+];
