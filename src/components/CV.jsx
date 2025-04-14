6733544dfa431a6724689a9d91189158c7c0f0fb
Web-Dev-Codi <cordiscobrian@gmail.com>
2025-04-14 11:32:03 +0200

    updated CV

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -1,13 +1,12 @@
 export const CV = () => {
-
   return (
     <div className="nav_buttons">
       <a
-        href="/files/BrianCordiscoCV_March2025.pdf"
+        href="/files/BrianCordiscoCVApril2025.pdf"
         target="_blank"
         rel="noopener noreferrer"
         className="resume_button"
-        download="BrianCordiscoCV_March2025.pdf"
+        download="BrianCordiscoCVApril2025.pdf"
       >
         Resume
       </a>
