7293b17359d1df498a3bc7dfbea65bfd5355e264
Web-Dev-Codi <cordiscobrian@gmail.com>
2025-04-21 15:37:45 +0200

    Update CV file name and download path to v2 version

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -2,11 +2,11 @@ export const CV = () => {
   return (
     <div className="nav_buttons">
       <a
-        href="/files/BrianCordiscoCVApril2025.pdf"
+        href="/files/BrianCordiscoCVApril2025-v2.pdf"
         target="_blank"
         rel="noopener noreferrer"
         className="resume_button"
-        download="BrianCordiscoCVApril2025.pdf"
+        download="BrianCordiscoCVApril2025-v2.pdf"
       >
         Resume
       </a>
