fd4f410719b37d5e4fc2547e47fdfb2c63d3670e
wev-dev-codi <cordiscobrian@gmail.com>
2026-07-27 10:41:18 +0200

    updated Cv

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -2,7 +2,7 @@ export const CV = () => {
   return (
     <div className="nav_buttons">
       <a
-        href="/files/BrianCordiscoCVApril2025.pdf"
+        href="/files/Brian_Cordisco_CV.pdf"
         target="_blank"
         rel="noopener noreferrer"
         className="resume_button"
